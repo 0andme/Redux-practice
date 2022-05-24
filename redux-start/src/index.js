3e3c193130dc4cc09ebd9b7bf7b0b635b7100969
0andme <yu00mi97@gmail.com>
2022-05-25 03:38:56 +0900

    feat : store 생성-getState-dispatch-subscribe

--- redux-start/src/index.js
+++ redux-start/src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./redux/store";
+import { addTodo } from "./redux/actions";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+store.dispatch(addTodo("red"));
+
+unsubscribe();
+store.dispatch(addTodo("2"));
+store.dispatch(addTodo("zz"));
 root.render(
   <React.StrictMode>
     <App />
--- redux-start/src/redux/reducers.js
+++ redux-start/src/redux/reducers.js
@@ -5,7 +5,7 @@ import { ADD_TODO } from "./actions.js";
 // state의 초기값
 const initialState = [];
 
-function todoApp(previousState = initialState, action) {
+export function todoApp(previousState = initialState, action) {
   if (action.type === ADD_TODO) {
     return [...previousState, action.todo];
   }
