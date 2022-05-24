e288ee60ad965af00c493a4098cbee2d8b92571b
0andme <yu00mi97@gmail.com>
2022-05-25 05:45:44 +0900

    feat : createContext적용-커스텀훅-컴포넌트추가

--- redux-start/src/App.js
+++ redux-start/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { addTodo } from "./redux/actions";
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
+import useReduxState from "./hooks/useReduxState";
+import useReduxDispatch from "./hooks/useReduxDispatch";
 function App() {
+  const state = useReduxState();
+  const dispatch = useReduxDispatch();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {JSON.stringify(state)}
+        <button onClick={click}>추가</button>
+        <TodoList />
+        <TodoForm />
       </header>
     </div>
   );
+
+  function click() {
+    dispatch(addTodo("할일"));
+  }
 }
 
 export default App;
--- redux-start/src/index.js
+++ redux-start/src/index.js
@@ -4,20 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
-import { addTodo, completeTodo, showAll, showComplete } from "./redux/actions";
+import ReduxContext from "./contexts/ReduxContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const unsubscribe = store.subscribe(() => {
-  console.log(store.getState());
-});
-store.dispatch(addTodo("할일"));
-store.dispatch(completeTodo(0));
-store.dispatch(showComplete());
-store.dispatch(showAll());
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ReduxContext.Provider value={store}>
+      <App />
+    </ReduxContext.Provider>
   </React.StrictMode>
 );
 
